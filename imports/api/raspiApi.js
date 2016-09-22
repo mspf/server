cec9ba65675a82f0434c2d00ab7dc1503bf5d650
parano <paranoyang@gmail.com>
2016-09-21 22:15:04 -0700

    api working

--- imports/api/raspiApi.js
+++ imports/api/raspiApi.js
@@ -1,16 +1,29 @@
 import { Meteor } from 'meteor/meteor';
+import bodyParser from 'body-parser';
 
 import { Questions } from './questions.js';
+import { Answers } from './answers.js';
 
-if (Meteor.isServer) {
-  const RasPiApi = new Restivus({
-    authRequired: false, // :P
-    prettyJson: true,
+Picker.middleware(bodyParser.json());
+Picker.middleware(bodyParser.urlencoded({ extended: false }));
+
+Picker.route('/questions', function(params, req, res) {
+  const questions = Questions.find({}, { sort: { createdAt: -1 } }).fetch();
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end(JSON.stringify(questions));
+});
+
+Picker.route('/results', function(params, req, res, next) {
+  assert(_.isArray(req.body))
+
+  req.body.forEach(answer => {
+    Answers.insert({
+      questionId: answer.questionId,
+      answer: answer.answer,
+      createdAt: new Date(answer.createdAt),
+    });
   });
 
-  RasPiApi.addRoute('questions', {
-    get: function() {
-      return Questions.find({}, { sort: { createdAt: -1 } });
-    }
-  });
-}
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end('OK');
+});
