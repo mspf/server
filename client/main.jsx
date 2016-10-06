9b16fec3eb8d52b3e2aa8e2bd4cb68b796af0af6
parano <paranoyang@gmail.com>
2016-10-05 19:03:12 -0700

    home view base

--- client/main.jsx
+++ client/main.jsx
@@ -4,9 +4,9 @@ import { render } from 'react-dom';
 
 import App from '/imports/ui/App.jsx';
 import Results from '/imports/ui/results.jsx';
+import HomeView from '/imports/ui/home.jsx';
 
 import { Router, browserHistory, Route, IndexRoute } from 'react-router';
-
 
 
 Meteor.startup(() => {
@@ -14,10 +14,10 @@ Meteor.startup(() => {
 
   render((
     <Router history={browserHistory} >
-      <Route path="/" component={Results} />
+      <Route path="/" component={HomeView} />
+      <Route path="/results" component={Results} />
       <Route path="/admin" component={App} />
       <Route path="*" component={Results} />
     </Router>
   ), document.getElementById('render-target'));
-
 });
---
+++ imports/ui/home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class HomeView extends React.Component {
+  render() {
+    return (
+      <h1>Hello, VWYF</h1>
+    );
+  }
+}
