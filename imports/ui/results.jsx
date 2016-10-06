723eefc8c85a441287a1c1ba5018c608715b7744
parano <paranoyang@gmail.com>
2016-10-05 22:35:55 -0700

    UI staff...

--- imports/ui/results.jsx
+++ imports/ui/results.jsx
@@ -1,36 +1,43 @@
 import React from 'react';
+import _ from 'lodash'
 import TrackerReact from 'meteor/ultimatejs:tracker-react';
 import { Questions } from '../api/questions.js';
 
 const ResultItem = ({question}) =>
-  (<div className="results-wrapper col-md-4">
+  (<div className='col-md-4'>
+    <div className='result-wrapper'>
       <h1>{question.text}</h1>
-      <div className="row">
-          <span>{question.optionA}</span>
-          <span>{question.optionB}</span>
+      <div className="">
+        <span>{question.optionA}</span>
+        <span>{question.optionB}</span>
+        {question.count && question.count.A || 0}
+        {question.count && question.count.B || 0}
       </div>
-      <div className="row">
-          {question.count && question.count.A || 0}
-          {question.count && question.count.B || 0}
-      </div>
+    </div>
+  </div>);
+
+const ResultsRow = ({questions}) => (
+  <div className='row results-row'>
+    {questions.map(question => <ResultItem key={question.id} question={question} />)}
   </div>);
 
 class Results extends TrackerReact(React.Component) {
 
   renderResults() {
     let questions = Questions.find().fetch();
-    return questions.map(question => <ResultItem key={question.id} question={question} />);
+    return _.chunk(questions, 3).map(
+      (threeQuestions, i) => <ResultsRow key={i} questions={threeQuestions} />);
   }
 
   render() {
     return (
-      <div className="container">
+      <div className="main container-fluid">
         <header>
           <a href='http://test.com/link_to_google_form' target='_blank'>SUBMIT QUESTIONS</a>
         </header>
-        <ul>
+        <div className='container all-results-wrapper'>
           {this.renderResults()}
-        </ul>
+        </div>
       </div>
     );
   }
