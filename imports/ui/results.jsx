0d205ace79f64c587fefda567e81e2406af93d8d
parano <paranoyang@gmail.com>
2016-10-29 22:25:00 -0700

    fix pie chart label

--- imports/ui/results.jsx
+++ imports/ui/results.jsx
@@ -30,7 +30,7 @@ class ResultItem extends React.Component {
       animation: false,
       responsive: true,
       scaleBeginAtZero: true,
-      // you don't have to define this here, it exists inside the global defaults
+      tooltipTemplate: "<%=label%>",
       legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
     }
 
