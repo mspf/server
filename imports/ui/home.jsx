9ba752e81087ba97b2c8b8b8da62c39b47841989
parano <paranoyang@gmail.com>
2016-11-05 16:56:18 -0700

    twitter timeline in home view

--- imports/ui/home.jsx
+++ imports/ui/home.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
 const TWITTER_PAGE_LINK = 'https://twitter.com/vwyf1x1';
+const GOOGLE_FORM_LINK = 'https://goo.gl/forms/lf9dBVNkjt0ixVmn2';
 
 export default class HomeView extends React.Component {
   render() {
     return (
-      <h1>Hello, VWYF</h1>
+      <div className="main container-fluid home">
+        <header>
+        </header>
+
+
+        <div className='container home-wrapper'>
+          <a className="twitter-timeline" data-width="800" data-height="300" data-theme="light" data-link-color="#2B7BB9" href="https://twitter.com/vwyf1x1"></a>
+        </div>
+      </div>
     );
   }
 }
