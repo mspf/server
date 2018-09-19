e20c3cad07702c9a70a2cac563fca53cabefd7d9
Chaoyu Yang <paranoyang@gmail.com>
2018-09-18 19:39:02 -0700

    mup file update, deployment fix

---
+++ .deploy/mup.js
@@ -0,0 +1,64 @@
+module.exports = {
+  servers: {
+    one: {
+      // TODO: set host address, username, and authentication method
+      host: '138.68.46.208',
+      username: 'root',
+      pem: '~/.ssh/id_rsa'
+      // password: 'server-password'
+      // or neither for authenticate from ssh-agent
+    }
+  },
+
+  app: {
+    name: 'VoteWithYourFeet',
+    path: '../',
+
+    servers: {
+      one: {},
+    },
+
+    buildOptions: {
+      serverOnly: true,
+    },
+
+    env: {
+      // TODO: Change to your app's url
+      // If you are using ssl, it needs to start with https://
+      ROOT_URL: 'http://138.68.46.208',
+      MONGO_URL: 'mongodb://mongodb/meteor',
+      MONGO_OPLOG_URL: 'mongodb://mongodb/local',
+    },
+
+    docker: {
+      //prepareBundle: false,
+      // change to 'abernix/meteord:base' if your app is using Meteor 1.4 - 1.5
+      //image: 'abernix/meteord:node-8.4.0-base',
+      image: 'abernix/meteord:base',
+    },
+
+    // Show progress bar while uploading bundle to server
+    // You might need to disable it on CI servers
+    enableUploadProgressBar: true
+  },
+
+  mongo: {
+    version: '3.4.1',
+    servers: {
+      one: {}
+    }
+  },
+
+  // (Optional)
+  // Use the proxy to setup ssl or to route requests to the correct
+  // app when there are several apps
+
+  // proxy: {
+  //   domains: 'mywebsite.com,www.mywebsite.com',
+
+  //   ssl: {
+  //     // Enable Let's Encrypt
+  //     letsEncryptEmail: 'email@domain.com'
+  //   }
+  // }
+};
