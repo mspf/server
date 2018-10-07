6ad935aab99926731ae5535655126fdeee1e1ede
Chaoyu Yang <paranoyang@gmail.com>
2018-10-07 11:04:29 -0700

    add static page

--- README.md
+++
@@ -1,9 +0,0 @@
-# mspf vwyf-server
-
-1. Install Meteor: `curl https://install.meteor.com/ | sh`
-2. Install node packages: `meteor npm install`
-3. Run the server: `npm run start`
-4. Access db: `npm run mongo`
-
-### Deploy application
-`npm run deploy` or `mup deploy`
--- data/export.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mongoexport --host localhost --db VoteWithYourFeet --collection answers --type=csv --out answers.csv --fields questionId,answer,createdAt
-
-mongoexport --host localhost --db VoteWithYourFeet --collection questions --type=csv --out questions.csv --fields _id,text,optionA,optionB,count
